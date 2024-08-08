db4ed3c6c84b3ca9459b4caacbd486a7589feef7
alex piku <122716205+Piku090909@users.noreply.github.com>
2024-08-08 23:28:06 +0530

    Update sticker.js

--- src/plugin/sticker.js
+++ src/plugin/sticker.js
@@ -6,8 +6,8 @@ const stickerCommand = async (m, gss) => {
   const prefix = prefixMatch ? prefixMatch[0] : '/';
   const [cmd, arg] = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ') : ['', ''];
 
-  const packname = global.packname || "𝐄𝐭𝐡𝐢𝐱-𝐌𝐃";
-  const author = global.author || "🥵💫👿";
+  const packname = global.packname || "𝛥𝐿𝛯𝛸 𝛲𝛪𝛫𝑈";
+  const author = global.author || "𝛥𝐿𝛯𝛸 𝛲𝛪𝛫𝑈 𝛭𝐷";
 
   const validCommands = ['sticker', 's', 'autosticker'];
 
